b914a32108d6dc61ea9a916f1d34e7d76ec48545
Santiago Sanchez <sjsatx08@gmail.com>
2024-03-28 11:34:44 -0500

    Added a placeholder menu

---
+++ main.py
@@ -0,0 +1,11 @@
+menu = '''
+~~~~~~~~~~~~~~~~~~~~~~~~~
+Welcome to the main menu!
+
+1. Place holder 1
+2. Place holder 2
+3. Place holder 3
+~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
+menu_choice = input(menu)
